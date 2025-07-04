f54942973b8135c0fcb689388e33d07145ac5724
niLPotential <niLPotential@proton.me>
2025-07-04 06:02:57 +0000

    Add normalizer

--- src/normalize-props.ts
+++ src/normalize-props.ts
@@ -1,3 +1,16 @@
 import { createNormalizer } from "@zag-js/types";
 
-export const normalizeProps = createNormalizer((props) => props);
+function toAlpineProp(prop: string) {
+  if (prop.startsWith("on")) return `@${prop.substr(2)}`;
+  return prop;
+}
+
+export const normalizeProps = createNormalizer((props: any) => {
+  return Object.entries(props).reduce<any>((acc, [key, value]) => {
+    if (value === undefined) return acc;
+
+    acc[toAlpineProp(key)] = value;
+
+    return acc;
+  }, {});
+});
