f6409d0d0e935a7d2efd27821a28e6d6e094eee7
niLPotential <niLPotential@proton.me>
2025-07-04 07:37:37 +0000

    Add accordion example

---
+++ example/pages/accordion.ts
@@ -0,0 +1,18 @@
+import Alpine from "alpinejs";
+import * as accordion from "@zag-js/accordion";
+import { normalizeProps, VanillaMachine } from "../../src/mod.ts";
+
+// @ts-ignore
+window.Alpine = Alpine;
+
+Alpine.data("accordion", () => {
+  const machine = new VanillaMachine(accordion.machine);
+  return {
+    init() {
+      machine.start();
+    },
+    api: accordion.connect(machine.service, normalizeProps),
+  };
+});
+
+Alpine.start();
