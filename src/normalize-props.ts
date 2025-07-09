8b495306d37e007da85016e983d65046033a6b8f
niLPotential <niLPotential@proton.me>
2025-07-09 18:47:48 +0900

    Bind props

--- src/normalize-props.ts
+++ src/normalize-props.ts
@@ -2,17 +2,14 @@ import { createNormalizer } from "@zag-js/types";
 
 export const normalizeProps = createNormalizer((props) => {
   const normalized: Record<string, any> = {};
-  for (let key in props) {
+  for (const key in props) {
     const value = props[key];
     if (!value) continue;
     if (key.startsWith("on")) {
-      key = `@${key.substring(2).toLowerCase()}`;
+      normalized[`@${key.substring(2).toLowerCase()}`] = value;
+    } else {
+      normalized[":" + key] = () => value;
     }
-    if (key === "hidden") {
-      normalized[":hidden"] = () => value;
-      continue;
-    }
-    normalized[key] = value;
   }
   return normalized;
 });
