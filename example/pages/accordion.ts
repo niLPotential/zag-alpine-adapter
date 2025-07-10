343659f36873b03aa5fbf0d8151f6f3de3980b19
niLPotential <niLPotential@proton.me>
2025-07-10 20:07:47 +0900

    Embed data directly

--- example/pages/accordion.ts
+++ example/pages/accordion.ts
@@ -3,24 +3,19 @@ import * as accordion from "@zag-js/accordion";
 import { AlpineService, normalizeProps } from "../../src/mod.ts";
 
 // @ts-ignore
-window.Alpine = Alpine;
+globalThis.Alpine = Alpine;
 
-class Accordion {
-  service: accordion.Service & { init: VoidFunction };
-  api: accordion.Api;
-  init: VoidFunction;
-  constructor() {
-    this.service = new AlpineService(accordion.machine, {
-      id: "1",
-      dir: "ltr",
-    });
-    this.api = accordion.connect(this.service, normalizeProps);
-    this.init = () => {
-      this.service.init();
-    };
-  }
-}
-
-Alpine.data("accordion", () => new Accordion());
+Alpine.data("accordion", () => ({
+  service: new AlpineService(accordion.machine, {
+    id: "1",
+    dir: "ltr",
+  }),
+  get api() {
+    return accordion.connect(this.service, normalizeProps);
+  },
+  init() {
+    this.service.init();
+  },
+}));
 
 Alpine.start();
