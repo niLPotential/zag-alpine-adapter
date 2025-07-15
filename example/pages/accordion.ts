8efb5e0304dd1b449168adcf39a42d646fc9a0a6
niLPotential <niLPotential@proton.me>
2025-07-15 00:45:35 +0000

    Testing

--- example/pages/accordion.ts
+++ example/pages/accordion.ts
@@ -1,9 +1,32 @@
 import Alpine from "alpinejs";
 import * as accordion from "@zag-js/accordion";
 import { normalizeProps, useMachine } from "../../src/mod.ts";
+import { createNormalizer } from "@zag-js/types";
 
 // @ts-ignore
 globalThis.Alpine = Alpine;
+
+Alpine.data("counter", () => {
+  const data = Alpine.reactive({ value: 1 });
+  const normalize = createNormalizer((props) => {
+    const normalized = {};
+    // const v = props.value; fails
+    normalized["x-text"] = () => props.value; // works
+    return normalized;
+  });
+  const connect = (data, normalize) => {
+    return normalize.button(data);
+  };
+  return {
+    bind: {
+      "x-bind": () => connect(data, normalize),
+      "@click": () => {
+        data.value++;
+        console.log(data.value);
+      },
+    },
+  };
+});
 
 Alpine.data("accordion", () => {
   const service = useMachine(accordion.machine, {
